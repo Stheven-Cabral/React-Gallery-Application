f2a4b21c4414e6c6ce00314610b5082b41718dd1
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-09 09:39:01 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,9 +5,9 @@ const Navigation = () => {
   return (
     <nav className="main-nav">
       <ul>
-        <li><NavLink exact to='/cats'>Cats</NavLink></li>
-        <li><NavLink exact to='/dogs'>Dogs</NavLink></li>
-        <li><NavLink exact to='/computers'>Computers</NavLink></li>
+        <li><NavLink exact to='/cats' activeClassName="nav-select">Cats</NavLink></li>
+        <li><NavLink exact to='/dogs' activeClassName="nav-select">Dogs</NavLink></li>
+        <li><NavLink exact to='/computers' activeClassName="nav-select">Computers</NavLink></li>
       </ul>
     </nav>
   )
